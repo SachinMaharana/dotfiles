4846167037e5e87aa285cdb132daeee9eb0cc423
SachinMaharana <sachin.nicky@gmail.com>
2020-09-13 17:22:32 +0530

    add update

--- .config/polybar/scripts/poly.py
+++ .config/polybar/scripts/poly.py
@@ -18,12 +18,14 @@ country = "us"
 
 try:
     data = requests.get('https://newsapi.org/v2/top-headlines?apiKey='+api_key+'&sources='+sources).json()
+    # d = requests.get('https://newsapi.org/v2/sources?apiKey=439c693305804e4d9afa86d9e2bad515').json()
 
     sourceName = data['articles'][0]['source']['name']
     title = data['articles'][0]['title']
     url = data['articles'][0]['url']
 
     print(sourceName+': '+title)
+    # print(d['sources'])
 
     path = os.path.join(save_path,"current_url.txt")         
     f = open(path, "w")
